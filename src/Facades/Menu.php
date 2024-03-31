673bd230e7770076e65c73cf10f24e2dcda85c3e
Remco Speekenbrink <98remco@gmail.com>
2024-04-01 00:43:05 +0100

    Add method declarations to facade doc

--- src/Facades/Menu.php
+++ src/Facades/Menu.php
@@ -5,6 +5,17 @@ namespace RSpeekenbrink\LaravelMenu\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \RSpeekenbrink\LaravelMenu\MenuItem add(string $name, string $route, array $attributes =  [])
+ * @method static \RSpeekenbrink\LaravelMenu\MenuItem addIf($condition, string $name, string $route, array $attributes = [])
+ * @method static \RSpeekenbrink\LaravelMenu\MenuItem addIfCan($authorization, string $name, string $route, array $attributes = [])
+ * @method static \RSpeekenbrink\LaravelMenu\MenuItem getItemByName($name)
+ * @method static int getIndexByName($name)
+ * @method static \RSpeekenbrink\LaravelMenu\MenuItemCollection getMenuItems()
+ * @method static array toArray()
+ * @method static void loadChildren(MenuItem $parent, Closure $items)
+ * @method static string toJson($options = 0)
+ * @method static mixed jsonSerialize()
+ *
  * @see \RSpeekenbrink\LaravelMenu\Menu
  */
 class Menu extends Facade
